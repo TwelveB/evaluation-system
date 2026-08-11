8e6662dc82f9dcd6a7b7fad7d805ab6da905c8b1
007_napat <67219010007@svc.ac.th>
2026-08-11 23:57:53 +0700

    add student login and jwt token and bcrypt compare

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const db = require('./db');
 require('dotenv').config();
@@ -9,6 +10,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const SECRET_KEY = process.env.SECRET_KEY || "1";
 
 // Test Route ดึงข้อมูลเวลาจาก PostgreSQL
 app.get('/api/students', async (req, res) => {
@@ -60,6 +63,59 @@ app.post('/api/students', async (req, res) => {
     
     const newStudent = await db.query(queryText, values);
     res.status(201).json(newStudent.rows[0]);
+
+  } catch (err) {
+    console.error('Database Connection Error:', err.message);
+
+    if (err.code === '23505') {
+      return res.status(400).json({ error: 'อีเมลนี้ถูกใช้งานแล้วในระบบ' });
+    }
+
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+//method post login
+app.post('/api/students/login', async (req, res) => {
+  try {
+    const { email, password} = req.body;
+    
+    if (!email || !password) {
+      return res.status(400).json({ error: 'กรุณากรอกข้อมูลให้ครบถ้วน' });
+    }
+    //ค้นหาแอคเคาท์ที่มีอีเมลเดียวกันกับที่ส่งมา
+    const result = await db.query(
+      'SELECT * FROM public."studentTB" WHERE email = $1',
+      [email]
+    );
+    
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: 'อีเมลหรือรหัสผ่านไม่ถูกต้อง' });
+    };
+
+    const student = result.rows[0];
+
+    const isMatch = await bcrypt.compare(password, student.password_hash)
+    if (!isMatch) {
+      return res.status(401).json({ error: 'อีเมลหรือรหัสผ่านไม่ถูกต้อง' });
+    }
+
+    //สร้าง JWT Token ส่งกลับไปฝั่ง Client
+    const token = jwt.sign(
+      { student_id: student.student_id, email: student.email },
+      SECRET_KEY,
+      { expiresIn: '1d' }
+    );
+
+    res.json({
+      message: 'เข้าสู่ระบบสำเร็จ',
+      token,
+      student: {
+        student_id: student.student_id,
+        first_name: student.first_name,
+        email: student.email
+      }
+    });
 
   } catch (err) {
     console.error('Database Connection Error:', err.message);
